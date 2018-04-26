e8df09a7d6fb0ce636092a1e20dac51c938b4df5
Guillaume Quiniou <guillom.quiniou@hotmail.fr>
2018-04-26 17:07:47 +0200

    gestion salons

---
+++ src/AppBundle/Controller/SalonController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Brand;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Admin;
+
+
+
+class SalonController extends Controller
+{
+    public function listSalonAction(){
+        $em = $this->getDoctrine()->getManager();
+
+        $salons = $em->getRepository('AppBundle:Salon')->findAll();
+        $countries  = $em->getRepository('AppBundle:Country')->findAll();
+
+        return $this->render('AppBundle::salons_boutiques.html.twig', array(
+            'salons' => $salons,
+            'countries' => $countries,
+        ));
+    }
+
+
+
+
+
+}
--- src/AppBundle/Entity/Salon.php
+++ src/AppBundle/Entity/Salon.php
@@ -13,6 +13,165 @@ use Doctrine\ORM\Mapping as ORM;
 class Salon
 {
     /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateSalon()
+    {
+        return $this->dateSalon;
+    }
+
+    /**
+     * @param \DateTime $dateSalon
+     */
+    public function setDateSalon($dateSalon)
+    {
+        $this->dateSalon = $dateSalon;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param string $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return int
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * @param int $zipcode
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param string $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * @param int $pays
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGImg()
+    {
+        return $this->gImg;
+    }
+
+    /**
+     * @param string $gImg
+     */
+    public function setGImg($gImg)
+    {
+        $this->gImg = $gImg;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPImg()
+    {
+        return $this->pImg;
+    }
+
+    /**
+     * @param string $pImg
+     */
+    public function setPImg($pImg)
+    {
+        $this->pImg = $pImg;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSalonId()
+    {
+        return $this->salonId;
+    }
+
+    /**
+     * @param int $salonId
+     */
+    public function setSalonId($salonId)
+    {
+        $this->salonId = $salonId;
+    }
+    /**
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255, nullable=false)
@@ -62,16 +221,16 @@ class Salon
     private $description;
 
     /**
-     * @var integer
+     * @var string
      *
-     * @ORM\Column(name="G_img", type="integer", nullable=false)
+     * @ORM\Column(name="G_img", type="string", length=255, nullable=false)
      */
     private $gImg;
 
     /**
-     * @var integer
+     * @var string
      *
-     * @ORM\Column(name="p_img", type="integer", nullable=false)
+     * @ORM\Column(name="p_img", type="string", length=255, nullable=false)
      */
     private $pImg;
 
