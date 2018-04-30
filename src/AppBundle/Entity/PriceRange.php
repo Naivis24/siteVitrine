a4ce5c5798223332a3a3451923d91647099c231e
Naivis <petitpepe650@yahoo.fr>
2018-04-30 11:34:58 +0200

    ouais :airplane:

--- src/AppBundle/Entity/PriceRange.php
+++ src/AppBundle/Entity/PriceRange.php
@@ -28,6 +28,38 @@ class PriceRange
      */
     private $id;
 
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
 
 }
 
