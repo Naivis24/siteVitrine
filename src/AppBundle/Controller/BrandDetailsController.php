2c02732004e048201bebf7225d740eee7eeeaea2
Naivis <petitpepe650@yahoo.fr>
2018-05-02 11:42:06 +0200

    début des marques recommandées

--- src/AppBundle/Controller/BrandDetailsController.php
+++ src/AppBundle/Controller/BrandDetailsController.php
@@ -11,13 +11,23 @@ use AppBundle\Entity\Brand;
 
 class BrandDetailsController extends Controller
 {
+    /**
+     *
+     * Cette fonction permet de retourner toutes les infos d'une marque à partir de son nom
+     * Retourne également la liste des marques pour permettre de voir les marques concurentes
+     *
+     * @param $marque : nom de la marque dont on veut les détails
+     * @return Response : brand => détails d'une marque, brands => liste des marques
+     */
     public function getOneBrandAction($marque){
         $em = $this->getDoctrine()->getManager();
 
         $brand = $em->getRepository('AppBundle:Brand')->findOneByName($marque);
+        $brands = $em->getRepository('AppBundle:Brand')->findAll();
+        $brandstyles = $em->getRepository('AppBundle:Style')->findAll();
 
         return $this->render('AppBundle::marque_details.html.twig', array(
-            'brand' => $brand,
+            'brand' => $brand, 'brands' => $brands, 'brandstyles' => $brandstyles,
         ));
     }
 
