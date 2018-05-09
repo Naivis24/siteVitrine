170fe11a5cc8609c54fe20327f561dc6b48c34a6
Guillaume Quiniou <guillom.quiniou@hotmail.fr>
2018-05-09 15:52:19 +0200

    filtre marques

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -2,27 +2,63 @@ function filterCb(obj, categorie, filtre) {
     var x, i;
     x = document.getElementsByClassName("filter-card");
 
+    if (filtre == "all") filtre = "";
+
     if ($(obj).is(":checked")) {
         for (i = 0; i < x.length; i++) {
-            if (x[i].getElementById(categorie).innerHTML.indexOf(filtre) != -1) addClass(x[i], "show");
+            if (x[i].getElementsByClassName(categorie)[0].innerHTML.indexOf(filtre) != -1) addClass(x[i], "show");
         }
     } else {
         for (i = 0; i < x.length; i++) {
-            removeClass(x[i], "show");
-            if (x[i].getElementById(categorie).innerHTML.indexOf(filtre) != -1) removeClass(x[i], "show");
+            if (x[i].getElementsByClassName(categorie)[0].innerHTML.indexOf(filtre) != -1) removeClass(x[i], "show");
         }
+    }
 
+}
+
+function filter(){
+    var cb = $("input:checkbox");
+    var tab = [];
+    var x,i;
+
+    filterReset();
+
+    $.each(cb, function(){
+         if(this.checked == true){
+             tab.push(this.value);
+         }
+    });
+    console.log(tab);
+    
+    var categories = [], filtres = [];
+    var temp;
+    for(i =0; i < temp.length; i++){
+        temp= tab[0].split(":");
+        categories.push(temp[0]);
+        filtres.push(temp[0]);
+    }
+    x = document.getElementsByClassName("filter-card");
+    for (i = 0; i < x.length; i++) {
+        if(x[i].getElementsByClassName(categorie)[0].innerHTML.indexOf(filtre) != -1)
+    }
+
+
+
+}
+function filterAll() {
+    var x;
+    x = document.getElementsByClassName("filter-card");
+
+    for (i = 0; i < x.length; i++) {
+        addClass(x[i], "show");
     }
 }
 
-
-function filterSelection(c) {
-    var x, i;
+function filterReset(){
+    var x;
     x = document.getElementsByClassName("filter-card");
-    if (c == "all") c = "";
     for (i = 0; i < x.length; i++) {
         removeClass(x[i], "show");
-        if (x[i].getElementById("cible").innerHTML.includes(c)) addClass(x[i], "show");
     }
 }
 
